3df8d14f47ee529cbfd4c6a2e5aeb7060241590f
Joseph Martin <josephmartin1350@gmail.com>
2022-02-22 18:56:45 -0700

    commit tree aa298ce7bc97ac924d3095d7eeea344464535f07

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize word
+        @word = word
+    end
+    def match array
+        new_array = Array.new
+        array.filter do |item| 
+            if item.chars.sort == word.chars.sort
+                if item == nil
+                    item.delete
+                else
+                    new_array << item
+                end
+            end
+        end
+    end
+end
